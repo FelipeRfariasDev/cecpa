d1c94aa8f1d5f06cb11093128dd0fd2abfa572b4
Cynthia <cynthiagcordeiro@gmail.com>
2022-01-31 09:16:14 -0300

    ajuste nas urls, conexao, menu

--- menu.php
+++ menu.php
@@ -1,3 +1,5 @@
-<a href="inicio.php">Inicio</a> |
+<a href="http://localhost/cecpa/inicio.php">Inicio</a> |
 <a href="http://localhost/cecpa/coordenacao/listar.php">Coordenação</a> |
-<a href="index.php">Sair</a>
+<a href="http://localhost/cecpa/funcao/listar.php">Função</a> |
+<a href="http://localhost/cecpa/index.php">Sair</a>
+
